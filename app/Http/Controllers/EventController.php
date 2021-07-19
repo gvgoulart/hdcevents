888199fd4fc7539099c571ccec9de25b71498521
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-07-19 17:17:26 -0300

    Salvando JSON no MySQL

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -24,6 +24,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         //Image upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
@@ -43,4 +44,12 @@ class EventController extends Controller
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 
+    public function show($id) {
+        
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+
+    }
 }
+
