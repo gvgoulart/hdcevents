a6db7fb516586d71e95d57910a29ec3a6e125269
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-07-19 14:36:02 -0300

    Finalizando a Home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,6 +44,6 @@
       <footer>
         <p>HDC Events &copy; 2020</p>
       </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     </body>
-    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 </html>
